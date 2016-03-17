77aa1faddf0479ab57ac9eb72689be4c7b4966b7
Michael Deiter <mdeiter@asu.edu>
2016-03-17 05:32:15 -0700

    added StringUtils, WeatherService, WebServices

---
+++ StringUtils/src/dev/code/StringUtils.java
@@ -0,0 +1,129 @@
+package dev.code;
+
+import java.lang.reflect.Method;
+
+import javax.print.attribute.standard.Chromaticity;
+
+public class StringUtils {
+
+	public static char getChar(String s, int pos) {
+		char chr = '\0';
+		chr = s.charAt(pos);
+		return chr;
+	}
+
+	public static boolean isPalindrome(String s) {
+		String palindrome = "";
+		String newStr = "";
+		boolean isPalindrome = false;
+		char chrF = '\0'; // forward
+		char chrB = '\0'; // backward
+		int k = 0, breakPos = 0, remainder = 0;
+		boolean isEven = false;
+
+		newStr = s.replaceAll("\\W", "");
+		newStr = newStr.toLowerCase();
+		k = newStr.length() - 1;
+
+		// If even number of chars, then palindrome must be split down middle
+		if (newStr.length() % 2 == 0) {
+			isEven = true;
+			breakPos = k / 2;
+		}
+
+		System.out.println("New string is: " + newStr);
+
+		for (int i = 0; i < newStr.length(); i++) {
+			chrF = getChar(newStr, i);
+			chrB = getChar(newStr, k);
+			if (chrF != chrB) {
+				/*
+				 * System.out.println("i is: " + i + " k is: " + k + " chrF " +
+				 * chrF + " chrB " + chrB); System.out.println("Err2 " + s +
+				 * " is not a palindrome!  Try again...");
+				 */
+				isPalindrome = false;
+				break;
+			}
+			isPalindrome = true; // will be set to false if not true so okay to
+									// always set here
+			// check before k is decremented
+			if (isEven && k == breakPos) {
+				break;
+			}
+			k--; // decrement k every loop
+		}
+		return isPalindrome;
+	}
+
+	// Recursive method
+	public static boolean isPalindromeR(String s) {
+		String palindrome = "";
+		String newStr = "";
+		boolean isPalindrome = false;
+		char chrF = '\0'; // forward
+		char chrB = '\0'; // backward
+		int k = 0, breakPos = 0, remainder = 0;
+		boolean isEven = false;
+
+		newStr = s.replaceAll("\\W", "");
+		newStr = newStr.toLowerCase();
+		k = newStr.length() - 1;
+
+		// If even number of chars, then palindrome must be split down middle
+		if (newStr.length() % 2 == 0) {
+			isEven = true;
+			breakPos = k / 2;
+		}
+
+		System.out.println("New string is: " + newStr);
+
+		for (int i = 0; i < newStr.length(); i++) {
+			chrF = getChar(newStr, i);
+			chrB = getChar(newStr, k);
+			if (chrF != chrB) {
+				/*
+				 * System.out.println("i is: " + i + " k is: " + k + " chrF " +
+				 * chrF + " chrB " + chrB); System.out.println("Err2 " + s +
+				 * " is not a palindrome!  Try again...");
+				 */
+				isPalindrome = false;
+				break;
+			}
+			isPalindrome = true; // will be set to false if not true so okay to
+									// always set here
+			// check before k is decremented
+			if (isEven && k == breakPos) {
+				break;
+			}
+			k--; // decrement k every loop
+		}
+		return isPalindrome;
+	}
+
+	public static String checkParens(String s) {
+		String result = "";
+		char chr = '\0';
+		int l = 0, r = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			chr = StringUtils.getChar(s, i);
+			switch (chr) {
+			case '(':
+				l++;
+				break;
+			case ')':
+				r++;
+				break;
+			default:
+				break;
+			}
+		}
+		if (r != l) {
+				result = "ERROR!!! Right parens are: " + r + ".  Left parens are: " + l;
+			} else {
+				result = "Correct! Right parens are: " + r + ".  Left parens are: " + l;
+			}
+		return result;
+	}
+}
---
+++ StringUtils/src/dev/code/Tester.java
@@ -0,0 +1,23 @@
+package dev.code;
+
+public class Tester {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "";
+
+		StringUtils su = new StringUtils();
+		s = "Was it Eliot's toilet I saw";
+		//s = "A man, a plan, a canal – Panama";
+		s = "Doc Note: I dissent. A fast never prevents a fatness. I diet on cod";
+		s = "palindromeemordnilap";
+		if (su.isPalindrome(s)) {
+			System.out.println(s + " is a palindrome!!!");
+		} else {
+			System.out.println(s
+					+ " is not a palindrome.  Please try again...");
+		}
+		
+		s = su.checkParens("(4+5) / (34/23))");
+		System.out.println(s);
+	}
+}
